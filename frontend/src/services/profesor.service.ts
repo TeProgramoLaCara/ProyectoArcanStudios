e95575122e330dfa406ddc311fba29dd100ba410
TeProgramoLaCara <joeldanielm@hotmail.com>
2026-05-20 15:59:43 +0200

    ultimo commit de practicas

--- frontend/src/services/profesor.service.ts
+++
@@ -1,6 +0,0 @@
-import { apiFetch } from "@/lib/api";
-
-export async function getProfesores() {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return apiFetch<any[]>("/profesor");
-}
